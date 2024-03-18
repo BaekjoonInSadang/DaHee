5b0565995a5e7c30da244963a74187c4d7bb2fff
DaHee06 <todayda1006@gmail.com>
2024-03-18 23:50:30 +0900

    백준 2840,10448

---
+++ src/ch04/b11005.java
@@ -0,0 +1,26 @@
+package ch04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+/**
+ * 11005 - 진법 변환2
+ *
+ * 아스키 코드 'A'의 경우 65, 'A'의 경우 97
+ */
+public class b11005 {
+    public static void main(String[] args) throws IOException {
+        //다른풀이
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//
+//        System.out.println(Integer.toString(a, b).toUpperCase());
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+    }
+}
