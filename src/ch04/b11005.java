18ac9a27e1baf11e08de767d14b44b126a8d29fa
DaHee06 <todayda1006@gmail.com>
2024-03-20 05:23:36 +0900

    백준 11005

--- src/ch04/b11005.java
+++ src/ch04/b11005.java
@@ -3,7 +3,9 @@ package ch04;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 /**
  * 11005 - 진법 변환2
@@ -12,15 +14,36 @@ import java.util.Scanner;
  */
 public class b11005 {
     public static void main(String[] args) throws IOException {
-        //다른풀이
-//        Scanner scanner = new Scanner(System.in);
-//        int a = scanner.nextInt();
-//        int b = scanner.nextInt();
-//
-//        System.out.println(Integer.toString(a, b).toUpperCase());
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        //내풀이
+//        StringBuilder sb = new StringBuilder();
+//
+//        while((N%B) < B){
+//            if((N%B) >= 10) sb.append((char)(N%B)-10+'A');
+//            else sb.append((N%B));
+//            N /= B;
+//        }
+//
+//        System.out.println(sb.reverse().toString());
+
+
+        //List 이용
+        ArrayList<Character> list = new ArrayList<>();
+
+        while(N>0){
+            if((N%B) >= 10) list.add((char) (N % B - 10 + 'A'));
+            else list.add((char) (N % B + '0')); //그냥 숫자출력
+            N /= B;
+        }
+
+        for (int i = list.size() - 1; i >= 0; i--) { // 맨처음부터 출력해야하므로
+            System.out.print(list.get(i));
+        }
 
     }
 }
