48cfa4d3d9f259cc8ef038f0f741518c5d4d98a9
DaHee06 <todayda1006@gmail.com>
2024-03-29 20:09:40 +0900

    1302 재 2910 모름

---
+++ src/ch05/b1302.java
@@ -0,0 +1,54 @@
+package ch05;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 1. 가장 많이 팔린 책
+ * 2. 많이 팔린 책이 여러개 일 경우 사전 순 출력
+ */
+public class b1302 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        String[] arr = new String[N];
+
+        for(int i = 0 ; i< N ; i++){
+            arr[i] = br.readLine();
+        }
+
+        HashMap<String, Integer> map = new HashMap<>();
+
+        for(int i = 0 ;i < N ; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
+            }else{
+                map.put(arr[i], 1);
+            }
+        }
+
+        int max = 0;
+        String str = "";
+
+        for (String s : map.keySet()) {
+            if(max < map.get(s)){
+                max = map.get(s);
+                str = s;
+            }
+            //max값이 같은 단어들이 여러개 나올 때 추가 조건 넣기
+            else if(max == map.get(s) && str.compareTo(s) > 0){
+                str = s;
+            }
+        }
+
+        System.out.println(str);
+
+
+    }
+}
--- src/ch05/b2910.java
+++ src/ch05/b2910.java
@@ -3,13 +3,15 @@ package ch05;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.StringTokenizer;
 
 /**
  *  빈도 정렬
  *
- *  1. 숫자를 자주 등장하는 순
- *  2. 빈도가 같다면 먼저 낭노 것이 앞에 있어야 함
+ *  1. 숫자를 자주 등장하는 빈도를 센다.
+ *  2. 빈도가 같다면 먼저 나온 것을 앞에 출력한다.
  *
  */
 public class b2910 {
@@ -27,6 +29,27 @@ public class b2910 {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
+        HashMap<Integer, Integer> map = new HashMap<>();
 
+        for(int i = 0 ;i < N ; i++){
+            if(map.containsKey(arr[i])){
+                map.put(arr[i], map.get(arr[i])+1);
+            }else{
+                map.put(arr[i], 1);
+            }
+        }
+
+        int max = 0 ; // 빈도수, value
+        int num = 0 ;
+        //빈도가 같다면 먼저 나온 것을 앞에 출력 -> ??
+        for (Integer i : map.keySet()) {
+            if(max < map.get(i)){
+                max = map.get(i);
+                num = i;
+            }
+        }
+        //모르겠음
+
+        System.out.println(num);
     }
 }
